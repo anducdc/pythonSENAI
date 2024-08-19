80d62a53a52aee61891857a2000d43a025220663
Andu <anducdc@hotmail.com>
2024-08-19 17:44:47 -0300

    adição dos exercícios

---
+++ img/aula04/areaTriangulo.py
@@ -0,0 +1,8 @@
+#primeiro algoritmo Python
+
+baseTri = float(input('informe o valor da base do triângulo\n'))
+
+alturaTri = float(input('informe o valor da altura do triângulo\n'))
+
+
+print('O valor da área do triângulo é', (baseTri * alturaTri)/2)
---
+++ img/aula04/deltaBhaskara.py
@@ -0,0 +1,6 @@
+realA = float(input('informe o valor de \'a\'\n'))
+realB = float(input('informe o valor de \'b\'\n'))
+realC = float(input('informe o valor de \'c\'\n'))
+
+
+print((realB ** 2) - ((4*realA) - realC))
