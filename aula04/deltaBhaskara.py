97cbdc20ab96b8a269e12609a7b2722d57578dfd
Andu <anducdc@hotmail.com>
2024-08-20 15:46:18 -0300

    finalização das atividades

--- aula04/deltaBhaskara.py
+++ aula04/deltaBhaskara.py
@@ -2,5 +2,4 @@ realA = float(input('informe o valor de \'a\'\n'))
 realB = float(input('informe o valor de \'b\'\n'))
 realC = float(input('informe o valor de \'c\'\n'))
 
-
-print((realB ** 2) - ((4*realA) - realC))
+print('Δ =', (realB ** 2) - ((4*realA) - realC))
