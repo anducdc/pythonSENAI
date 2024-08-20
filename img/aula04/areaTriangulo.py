947f9fe358ed187694878f1317f9d05239e3f601
Andu <anducdc@hotmail.com>
2024-08-20 14:15:27 -0300

    trinângulo invertido

--- img/aula04/areaTriangulo.py
+++ img/aula04/areaTriangulo.py
@@ -5,4 +5,9 @@ baseTri = float(input('informe o valor da base do triângulo\n'))
 alturaTri = float(input('informe o valor da altura do triângulo\n'))
 
 
+print('           /|')
+print('         x/ |',alturaTri)
+print('         /__|')
+print('        ',baseTri )
+
 print('O valor da área do triângulo é', (baseTri * alturaTri)/2)
