4e563069e13ad4e979c3a93a1514d693d21e19d8
Anderson Alves <109828365+anducdc@users.noreply.github.com>
2024-08-29 13:01:25 -0300

    Create 58.py

---
+++ exercíciosAula_06/58.py
@@ -0,0 +1,6 @@
+palavra = str(input('Digite uma palavra para armazenar. Digite \'sair\' para terminar\n'))
+
+while palavra != 'sair':
+  palavra = str(input('Salvo! Digite outra palavra para prosseguir ou \'sair\' para encerrar o processo:\n'))
+else:
+  print('Programa finalizado')
