b2a82b0ffb4a140647d62a8496bd2ed431b75fd3
Anderson Alves <109828365+anducdc@users.noreply.github.com>
2024-08-29 12:51:09 -0300

    Update 56.py

--- exercíciosAula_06/56.py
+++ exercíciosAula_06/56.py
@@ -1,1 +1,6 @@
-numeroVezes = int(input())
+mensagem = 0
+numeroVezes = int(input('Quantas vezes você deseja que a mensagem se repita?\n'))
+
+while mensagem < numeroVezes:
+  print('Hello World')
+  mensagem +=1
